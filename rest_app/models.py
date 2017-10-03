7c1fef7b26d8ad8551efef3772ede137933b3e57
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2017-10-03 08:35:38 +0100

    Add an ECDSA + AES based crypto library

--- rest_app/models.py
+++ rest_app/models.py
@@ -10,7 +10,7 @@ class Group(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=80)
 
-    key = models.CharField(max_length=crypto.B64_KEY_LENGTH)
+    key = models.CharField(max_length=crypto.SERIALIZED_KEY_LENGTH)
     # owner_email = models.EmailField(max_length=254)
     # TODO: add proxy/name server address
     # TODO: add currency type
@@ -20,7 +20,7 @@ class User(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     # the uuid is the signing key of the user!
-    key = models.CharField(primary_key=True, max_length=crypto.B64_KEY_LENGTH)
+    key = models.CharField(primary_key=True, max_length=crypto.SERIALIZED_KEY_LENGTH)
 
     # if the user, after simplification, is a borrower
     balance = models.IntegerField(default=0)
